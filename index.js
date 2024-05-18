f6e898c397dbc71ffeda2f5385ef17fc7643127a
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-05-18 16:54:45 +0200

    Finished the convert function

---
+++ index.js
@@ -0,0 +1,12 @@
+let inputConvertElement = document.querySelector("#dollar-input");
+let inputRandElement = document.querySelector("#rand-input");
+
+function convertNow(event) {
+  event.preventDefault();
+  let dollarValue = parseFloat(inputConvertElement.value); //this line retrieves the value from the input field to float value
+  let randValue = dollarValue * 18.33;
+  inputRandElement.value = Math.floor(randValue);
+}
+
+let buttonConvertElement = document.querySelector("#convert-button");
+buttonConvertElement.addEventListener("click", convertNow);
